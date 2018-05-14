be69e539d7cd7be8b7283e9599a20841189f1a80
Garrett Kizior <kiziorgarrett@gmail.com>
2018-05-14 13:15:34 -0500

    changed textboxes to change events

--- abf-statracker/src/app/stats-list/stats-list.component.ts
+++ abf-statracker/src/app/stats-list/stats-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import {FormControl} from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/observable/fromEvent';
 
 @Component({
   selector: 'app-stats-list',
@@ -24,8 +28,10 @@ export class StatsListComponent implements OnInit {
   }
 
   onChange(key: string, category: string, value: string) {
-    const elementValue = (<HTMLInputElement>document.getElementById(value)).value;
+    const textInput =  (<HTMLInputElement>document.getElementById(value + '' + key));
+    const elementValue = textInput.value;
     const change = JSON.parse('{ "' + category + '": "' + elementValue + '"}');
     const addedStat = this.db.object('/stats/' + key).update(change);
   }
+
 }
